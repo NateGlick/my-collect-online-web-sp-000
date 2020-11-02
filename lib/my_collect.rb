f125649cbfdd1959587d31c26539b4e2e5666792
Nathan Glick <nateleighglick@hotmail.com>
2020-11-02 09:45:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   counter = 0
   array = []
   while counter < array.length
-  array << yield(array[counter]).upcase
+  array << yield(array[counter])
   counter += 1
  end
-array 
+array.upcase
 end
